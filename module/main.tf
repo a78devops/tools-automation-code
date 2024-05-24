d25c9fb177f47860355f1d4db3822badfaff852a
pajjuriabhishek <pajjuriabhishek55@gmail.com>
2024-05-24 19:48:36 +0530

    adding instance profile

--- module/main.tf
+++ module/main.tf
@@ -37,3 +37,8 @@ resource "aws_iam_role" "role" {
     name = "${var.tool_name}-role"
   }
 }
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.tool_name}-role"
+  role = aws_iam_role.role.name
+}
